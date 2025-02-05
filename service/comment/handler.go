b2455c45f5ea0da4227e9cdb0be9c1176bbd4932
yuxialuozi <3159452953@qq.com>
2025-02-05 18:18:36 +0800

    feat:comment's handler

--- service/comment/handler.go
+++ service/comment/handler.go
@@ -2,26 +2,328 @@ package main
 
 import (
 	"context"
+	"github.com/cloudwego/kitex/client"
+	"github.com/kitex-contrib/obs-opentelemetry/tracing"
+	consul "github.com/kitex-contrib/registry-consul"
+	"github.com/sirupsen/logrus"
+	"log"
+	"simpledouyin/config"
+	"simpledouyin/constants/biz"
 	comment "simpledouyin/kitex_gen/douyin/comment"
+	"simpledouyin/kitex_gen/douyin/user"
+	"simpledouyin/kitex_gen/douyin/user/userservice"
+	"simpledouyin/logging"
+	gen "simpledouyin/repo"
+	"simpledouyin/repo/model"
 )
+
+var UserClient userservice.Client
+
+func init() {
+	r, err := consul.NewConsulResolver(config.EnvConfig.CONSUL_ADDR)
+	if err != nil {
+		log.Fatal(err)
+	}
+	UserClient, err = userservice.NewClient(
+		config.UserServiceName,
+		client.WithResolver(r),
+		client.WithSuite(tracing.NewClientSuite()),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 // CommentServiceImpl implements the last service interface defined in the IDL.
 type CommentServiceImpl struct{}
 
 // ActionComment implements the CommentServiceImpl interface.
 func (s *CommentServiceImpl) ActionComment(ctx context.Context, req *comment.ActionCommentRequest) (resp *comment.ActionCommentResponse, err error) {
-	// TODO: Your code here...
+	logger := logging.Logger.WithFields(logrus.Fields{
+		"user_id":      req.ActorId,
+		"video_id":     req.VideoId,
+		"action_type":  req.ActionType,
+		"comment_text": req.GetCommentText(),
+		"comment_id":   req.GetCommentId(),
+		"function":     "ActionComment",
+	})
+	logger.Debugf("Process start")
+
+	var pCommentText string
+	var pCommentID uint32
+	switch req.ActionType {
+	case comment.ActionCommentType_ACTION_COMMENT_TYPE_ADD:
+		pCommentText = req.GetCommentText()
+	case comment.ActionCommentType_ACTION_COMMENT_TYPE_DELETE:
+		pCommentID = req.GetCommentId()
+	case comment.ActionCommentType_ACTION_COMMENT_TYPE_UNSPECIFIED:
+		fallthrough
+	default:
+		logger.Warnf("invalid action type")
+		return &comment.ActionCommentResponse{
+			StatusCode: biz.InvalidCommentActionType,
+			StatusMsg:  &biz.BadRequestStatusMsg,
+		}, nil
+	}
+
+	// Video check: check if the qVideo exists || check if creator is the same as actor
+	qVideo := gen.Q.Video
+	pVideo, err := qVideo.WithContext(ctx).
+		Where(qVideo.ID.Eq(req.VideoId)).
+		First()
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Warnf("video query error")
+		return &comment.ActionCommentResponse{
+			StatusCode: biz.UnableToQueryVideo,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}, nil
+	}
+	if pVideo == nil {
+		logger.Warnf("video not found")
+		return &comment.ActionCommentResponse{
+			StatusCode: biz.VideoNotFound,
+			StatusMsg:  &biz.BadRequestStatusMsg,
+		}, nil
+	}
+
+	userResponse, err := UserClient.GetUser(ctx, &user.UserRequest{
+		UserId:  req.ActorId,
+		ActorId: req.ActorId,
+	})
+
+	if err != nil || userResponse.StatusCode != biz.OkStatusCode {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Warnf("user service error")
+		return &comment.ActionCommentResponse{
+			StatusCode: biz.UnableToQueryUser,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}, nil
+	}
+
+	pUser := userResponse.User
+
+	switch req.ActionType {
+	case comment.ActionCommentType_ACTION_COMMENT_TYPE_ADD:
+		resp, err = addComment(ctx, logger, pUser, pVideo.ID, pCommentText)
+	case comment.ActionCommentType_ACTION_COMMENT_TYPE_DELETE:
+		resp, err = deleteComment(ctx, logger, pUser, pVideo.ID, pCommentID)
+	}
+	if err != nil {
+		return resp, err
+	}
+
+	logger.WithFields(logrus.Fields{
+		"response": resp,
+	}).Debug("all process done, ready to launch response")
+
+	return resp, err
+}
+
+func addComment(ctx context.Context, logger *logrus.Entry, pUser *user.User, pVideoID uint32, pCommentText string) (resp *comment.ActionCommentResponse, err error) {
+	count, err := count(ctx, pVideoID)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Debug("failed to query db entry")
+		resp = &comment.ActionCommentResponse{
+			StatusCode: biz.UnableToQueryComment,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}
+		return
+	}
+
+	rComment := model.Comment{
+		VideoId:   pVideoID,
+		CommentId: count + 1,
+		UserId:    pUser.Id,
+		Content:   pCommentText,
+	}
+
+	err = gen.Q.Comment.WithContext(ctx).Create(&rComment)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Debug("failed to create db entry")
+		resp = &comment.ActionCommentResponse{
+			StatusCode: biz.UnableToCreateComment,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}
+		return
+	}
+	resp = &comment.ActionCommentResponse{
+		StatusCode: biz.OkStatusCode,
+		StatusMsg:  &biz.OkStatusMsg,
+		Comment: &comment.Comment{
+			Id:         rComment.CommentId,
+			User:       pUser,
+			Content:    rComment.Content,
+			CreateDate: rComment.CreatedAt.Format("01-02"),
+		},
+	}
+	return
+}
+
+func deleteComment(ctx context.Context, logger *logrus.Entry, pUser *user.User, videoID uint32, commentID uint32) (resp *comment.ActionCommentResponse, err error) {
+	qComment := gen.Q.Comment
+
+	rComment, err := qComment.WithContext(ctx).
+		Where(qComment.VideoId.Eq(videoID), qComment.CommentId.Eq(commentID)).
+		First()
+
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Debug("failed to query db entry")
+		resp = &comment.ActionCommentResponse{
+			StatusCode: biz.UnableToQueryComment,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}
+		return
+	}
+
+	if rComment.UserId != pUser.Id {
+		logger.Debug("comment creator and actor not match")
+		return &comment.ActionCommentResponse{
+			StatusCode: biz.ActorIDNotMatch,
+			StatusMsg:  &biz.ForbiddenStatusMsg,
+		}, nil
+	}
+
+	_, err = qComment.WithContext(ctx).
+		Where(qComment.ID.Eq(rComment.ID)).
+		Delete()
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Debug("failed to delete db entry")
+		resp = &comment.ActionCommentResponse{
+			StatusCode: biz.UnableToDeleteComment,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}
+		return
+	}
+	resp = &comment.ActionCommentResponse{
+		StatusCode: biz.OkStatusCode,
+		StatusMsg:  &biz.OkStatusMsg,
+		Comment:    nil,
+	}
 	return
 }
 
 // ListComment implements the CommentServiceImpl interface.
 func (s *CommentServiceImpl) ListComment(ctx context.Context, req *comment.ListCommentRequest) (resp *comment.ListCommentResponse, err error) {
-	// TODO: Your code here...
-	return
+	logger := logging.Logger.WithFields(logrus.Fields{
+		"user_id":  req.ActorId,
+		"video_id": req.VideoId,
+		"function": "ListComment",
+	})
+	logger.Debug("Process start")
+
+	qVideo := gen.Q.Video
+	pVideo, err := qVideo.WithContext(ctx).
+		Where(qVideo.ID.Eq(req.VideoId)).
+		First()
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Debug("video query error")
+		return &comment.ListCommentResponse{
+			StatusCode: biz.UnableToQueryVideo,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}, nil
+	}
+	if pVideo == nil {
+		logger.Debug("video not found")
+		return &comment.ListCommentResponse{
+			StatusCode: biz.VideoNotFound,
+			StatusMsg:  &biz.BadRequestStatusMsg,
+		}, nil
+	}
+
+	qComment := gen.Q.Comment
+	pCommentList, err := qComment.WithContext(ctx).
+		Where(qComment.VideoId.Eq(pVideo.ID)).
+		Order(qComment.CreatedAt.Desc()).
+		Find()
+
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Debug("comment query error")
+		return &comment.ListCommentResponse{
+			StatusCode: biz.UnableToQueryComment,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}, nil
+	}
+
+	rCommentList := make([]*comment.Comment, 0, len(pCommentList))
+	for _, pComment := range pCommentList {
+		userResponse, err := UserClient.GetUser(ctx, &user.UserRequest{
+			UserId:  pComment.UserId,
+			ActorId: req.ActorId,
+		})
+
+		if err != nil || userResponse.StatusCode != biz.OkStatusCode {
+			logger.WithFields(logrus.Fields{
+				"pComment": pComment,
+				"err":      err,
+			}).Debug("unable to get user info")
+		}
+
+		rCommentList = append(rCommentList, &comment.Comment{
+			Id:         pComment.CommentId,
+			User:       userResponse.User,
+			Content:    pComment.Content,
+			CreateDate: pComment.CreatedAt.Format("01-02"),
+		})
+	}
+
+	logger.WithFields(logrus.Fields{
+		"response": resp,
+	}).Debug("all process done, ready to launch response")
+	return &comment.ListCommentResponse{
+		StatusCode:  biz.OkStatusCode,
+		StatusMsg:   &biz.OkStatusMsg,
+		CommentList: rCommentList,
+	}, nil
 }
 
 // CountComment implements the CommentServiceImpl interface.
 func (s *CommentServiceImpl) CountComment(ctx context.Context, req *comment.CountCommentRequest) (resp *comment.CountCommentResponse, err error) {
-	// TODO: Your code here...
-	return
+	logger := logging.Logger.WithFields(logrus.Fields{
+		"user_id":  req.ActorId,
+		"video_id": req.VideoId,
+		"function": "CountComment",
+	})
+	logger.Debug("Process start")
+
+	rCount, err := count(ctx, req.VideoId)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+		}).Debug("failed to count comment")
+		return &comment.CountCommentResponse{
+			StatusCode: biz.UnableToQueryComment,
+			StatusMsg:  &biz.InternalServerErrorStatusMsg,
+		}, nil
+	}
+
+	logger.WithFields(logrus.Fields{
+		"response": resp,
+	}).Debug("all process done, ready to launch response")
+	return &comment.CountCommentResponse{
+		StatusCode:   biz.OkStatusCode,
+		StatusMsg:    &biz.OkStatusMsg,
+		CommentCount: rCount,
+	}, nil
+}
+
+func count(ctx context.Context, videoId uint32) (count uint32, err error) {
+	rCount, err := gen.Q.Comment.WithContext(ctx).
+		Where(gen.Q.Comment.VideoId.Eq(videoId)).
+		Count()
+	return uint32(rCount), err
 }
